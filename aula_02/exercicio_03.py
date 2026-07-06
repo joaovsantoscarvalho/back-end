5a22f12ce8126d42444a26befab1bab2a4570b4f
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2026-07-06 10:53:13 +0000

    .

---
+++ aula_02/exercicio_03.py
@@ -0,0 +1,12 @@
+"""Exercício 03.
+
+Soma de Dois Números
+Desenvolva um programa que solicite dois números ao usuário e mostre o resultado da soma
+desses números.
+"""
+
+num_1 = int(input("Digite o 1º número: "))
+num_2 = int(input("Digite o 2º número: "))
+
+soma = num_1 + num_2
+print(soma)
---
+++ aula_03/testes_03.py
@@ -0,0 +1,7 @@
+# numero = input("Digite um número: ")
+# print(numero * 3)
+# print("5" * 3)
+# print("Batman" * 3)
+
+# numero = float(input("Digite um número: "))
+# print(numero * 3)
