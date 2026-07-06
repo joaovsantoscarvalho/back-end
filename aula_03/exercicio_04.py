7059181a7d5836f0ec697f1ca46378fda8e0163e
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2026-07-06 11:02:31 +0000

    ,

---
+++ aula_03/exercicio_04.py
@@ -0,0 +1,14 @@
+"""Exercício 04.
+
+Média das Notas
+Faça um programa que peça quatro notas bimestrais e apresente a média delas.
+"""
+
+n1 = int(input("Digite a 1º nota: "))
+n2 = int(input("Digite a 2º nota: "))
+n3 = int(input("Digite a 3º nota: "))
+n4 = int(input("Digite a 4º nota: "))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print(media)
