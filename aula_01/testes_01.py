24869f1d40c22c656361cb5d9d211f183836794e
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2026-07-06 10:36:26 +0000

    .

--- aula_01/testes_01.py
+++ aula_01/testes_01.py
@@ -1,41 +1,53 @@
-# # nome_escola = "Julio Mesquita"
-# # print(nome_escola)
+# nome_escola = "Júlio Mesquita"
+# print(nome_escola)
 
+# idade = 17
+# x = 52
 
-# # idade = 17
-# # x = 52
-
-# # valor bruto das vendas
-# bruto = 100
-# # valor bruto das vendas 
+# # Valor bruto das vendas
+# bruto = 1000
+# # Valor cobrado de imposto sobre as vendas
 # imposto = 200
-# valor liquido pos as vendas 
+# # Valor líquido após as vendas
 # liquido = bruto - imposto
 # print(liquido)
 
-# idade = 17
-# preco = 19,95
-# nome = maria
-# verdadeiro = true
-# falso = false
-# nulo = none
+idade = 17  # int
+preco = 19.95  # float
+nome = "Maria"  # str
+verdadeiro = True  # bool
+falso = False  # bool
+nulo = None  # null
 
 # numero = 10
-# resultado = numero * 5 
-
-# a = 20 
-# b = "batmam"
-# c = a + b
+# resultado = numero * 5
 
 # a = 20
-# b = "10"
+# b = "Batman"
+# c = a + b
+
+# a = 20  # int
+# b = "10"  # str
+# c = a + int(b)  # 30 (int)
+# print(c)
+
+# a = 20  # int
+# b = "10"  # str
+# c = str(a) + b  # "2010" (str)
+# print(c)
+# print("Abacate" + "Batman")  # "AbacateBatman" (str)
+
+# a = 20
+# b = "Batman"
 # c = a + int(b)
 
 # resultado = 5 * 3
 # print(resultado)
 
-nome_usuario = input("Digite seu nome: ")
-print("olá", nome_usuario)
+# nome_usuario = input("Digite seu nome: ")
+# print("Olá", nome_usuario)
 
-numero = int(input("Digite um numero: "))
+numero = int(input("Digite um número: "))
 print(numero * 3)
+
+# print("Batman" * 3)
